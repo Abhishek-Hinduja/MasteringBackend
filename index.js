2e7105dbe1642821524617c7081156d33716f5a1
Abhishek12589 <hindujaabhishek5@gmail.com>
2023-05-06 15:29:42 +0530

    Modules in node

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const http = require("http");
+import http from "http";
+import { lovecalculator } from "./feature.js";
 
 
 const server = http.createServer((req,res) => {
     if ((req.url) === "/"){
-        res.end("Welcome to the home page")
+        res.end(`<h1>love percentage is ${lovecalculator()}</h1>`)
     }
     else if ((req.url) === "/about"){
         res.end("Welcome to the About page")
@@ -16,8 +17,7 @@ const server = http.createServer((req,res) => {
     }
     else{
         res.end("Invalid url")
-    }  
-    console.log(req.url)
+    }
 })
 
 server.listen(5000, ()=>{
