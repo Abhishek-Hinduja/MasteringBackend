204e5620223a3403daec52c6005e8fdb4763fe09
Abhishek12589 <hindujaabhishek5@gmail.com>
2023-05-07 20:19:20 +0530

    Starting the express Server

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-import http from "http";
-import { lovecalculator } from "./feature.js";
-import fs from "fs";
-
-const home = fs.readFileSync("./index.html", ()=>{
-    console.log("hello")
-})
+import express from "express";
 
 
-const server = http.createServer((req,res) => {
-    if ((req.url) === "/"){
-        res.end(`<h1>love percentage is ${lovecalculator()}</h1>`)
-    }
-    else if ((req.url) === "/about"){
-        fs.readFileSync("./index.html", (err,data)=>{
-            res.end(data)
-        })
-    }
-    else if ((req.url) === "/contact"){
-        res.end("Welcome to the contact page")
-    }
-    else if ((req.url) === "/favicon.ico"){
-        res.end("Welcome to the cot page")
-    }
-    else{
-        res.end("Invalid url")
-    }
-})
+const server = express()
 
-server.listen(5000, ()=>{
-    console.log("Server is listening");
+server.get("/", (req,res)=>{
+    res.end("Hello you are Welcome In this so much solf loving self conscious word of Abhishek")
 });
+
+server.listen(4000, ()=>{
+    console.log("Server is listening")
+})
