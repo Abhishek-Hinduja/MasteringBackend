cdb1c2efe02091bf1726a5bf7de9b20844e1d73b
Abhishek12589 <hindujaabhishek5@gmail.com>
2023-05-08 17:25:32 +0530

    post method

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-import express from "express";
+import { Console } from "console";
+import express, { urlencoded } from "express";
 import fs from "fs";
 import path from "path";
 
 const app = express()
+const arr = []
 
 app.set("view engine", "ejs")
 
 app.use(express.static(path.join(path.resolve(), "public")))
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req,res)=>{
-    const data = {
-        name : "Abhishek",
-        department : "CSE"
-    }
-    res.render("index", data)
+    res.render("index")
 });
+
+app.get("/json", (req,res)=>{
+    res.json({
+        arr
+    })
+})
+
+app.post("/", (req,res)=>{
+    arr.push({
+        name : req.body.name,
+        email : req.body.email
+    })
+    res.render("success", {arr})
+})
 
 app.listen(4000, ()=>{
     console.log("Server is listening")
